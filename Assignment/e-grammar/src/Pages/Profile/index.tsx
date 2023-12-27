ca7e24074055f2862541e9d74789d9b8c9734bb7
ntn1205 <jackngo1205@gmail.com>
2023-12-28 02:04:15 +0700

    features: Profile

--- Assignment/e-grammar/src/Pages/Profile/index.tsx
+++ Assignment/e-grammar/src/Pages/Profile/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import userIcon from "../../assets/user.png";
+import userIcon from "../../assets/user-profile.png";
+import "./Profile.css";
 
 const Profile: React.FC = () => {
   const [isHovered, setIsHovered] = useState(false);
@@ -12,16 +13,35 @@ const Profile: React.FC = () => {
     setIsHovered(false);
   };
 
+  const username = "Nguyen Ngo";
+  const email = "nguyenngo@gmail.com";
+  const subDetail = "Premium";
+
   return (
     <div className="container">
-      <div
-        className={`profile-avatar-button ${isHovered ? "hovered" : ""}`}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      >
-        <input type="file" accept="image/*" style={{ display: "none" }} />
-        <img src={userIcon} alt="Upload Avatar" className={`avatar ${isHovered ? "hovered" : ""}`} />
-        {isHovered && <span className="text">Change Avatar</span>}
+        <h1>Profile</h1> 
+        <div className="profile-avatar-button">
+            <label
+                className={`avatar-button ${isHovered ? "hovered" : ""}`}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            >
+            <input type="file" accept="image/*" style={{ display: "none" }} />
+            <img
+                src={userIcon}
+                alt="Upload Avatar"
+                className="avatar"
+                style={{ width: "200px", height: "auto" }}
+            />
+            {isHovered && <span className="text">Change Avatar</span>}
+            </label>
+      </div>
+      <div className="user-details">
+        <div className="username">Name: {username}</div>
+        <div className="email">Email: {email}</div>
+        <div className="subDetail">
+          Subscription: <span className="subDetail-text">{subDetail}</span>
+        </div>
       </div>
     </div>
   );
