0b582d36fa263db6364b1b554e5ac049dc41741e
[An pham] <thinnphm826@gmail.com>
2024-01-13 13:40:13 +0700

    Add some comment

--- Assignment/src/main/java/Architecture/Assignment/controller/SystemController.java
+++ Assignment/src/main/java/Architecture/Assignment/controller/SystemController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SystemController {
 
+    //api to go to register page
     @GetMapping("/auth/register")
     public String registration(){
         return "registerPage";
     }
 
+    //Api to go to login page
     @GetMapping("/auth/login")
     public String login() {
         return "login";
